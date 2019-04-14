aaa89505f9f3c1b2a646d09c35be7e49af6ced67
living42 <qzeqing@gmail.com>
2019-04-14 14:15:20 +0800

    break down pieces

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 // TODO Remove ts-node dependency
-console.log("jms-connect 1.0.0");
+console.log('jms-connect 1.0.0')
 
 require('ts-node').register()
 
-require('./index').run()
+require('./main').main()
---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import inquirer from 'inquirer'
+import Term, { ITreeNode } from './term'
+
+export async function main() {
+    const nodeName = process.argv[2]
+
+    const { serverUrl } = await inquirer.prompt<{ serverUrl: string }>({
+        name: 'serverUrl',
+        message: 'set your jumpserver:',
+        default: 'http://localhost:8080'
+    })
+
+    const term = new Term(serverUrl)
+    await term.login()
+    await term.establishConnection()
+
+    const assets = (await term.getAssets()).filter(node => node.meta.type === 'asset')
+
+    let asset
+    if (!nodeName) {
+        const answer = await inquirer.prompt<{ asset: ITreeNode }>({
+            type: 'list',
+            name: 'asset',
+            message: 'choose asset to connect:',
+            choices: assets.map(x => ({ name: x.name, value: x }))
+        })
+        asset = answer.asset
+    } else {
+        asset = assets.filter(node => node.name === nodeName)[0]
+    }
+
+    if (!asset) {
+        console.error('asset you specified no found')
+        process.exit(1)
+    }
+
+    await term.connect(asset)
+}
